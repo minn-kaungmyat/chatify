d6b9a7058479bc291f43e679ada4b61d4f6f87cc
minn-kaungmyat <minnkaungmyat00@gmail.com>
2026-02-02 21:47:16 +0700

    message routes added

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,19 @@
 import express from "express";
+import {
+  getAllContacts,
+  getAllChats,
+  getMessagesByUserId,
+  sendMessage,
+} from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
+router.use(arcjetProtection, protectRoute);
 
-router.get("/send", (req, res) => {
-  res.send("Send message endpoint");
-});
+router.get("/contacts", getAllContacts);
+router.get("/chats", getAllChats);
+router.get("/:id", getMessagesByUserId);
+router.post("/send/:id", sendMessage);
 
 export default router;
