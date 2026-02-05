a964f341c37d0b042ce56a1b499741235b9508bd
minn-kaungmyat <minnkaungmyat00@gmail.com>
2026-02-05 18:18:42 +0700

    send message functionality

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -17,10 +17,18 @@ function ChatContainer() {
     }
   }, [selectedUser, getMessagesByUserId]);
 
+  useEffect(() => {
+    const chatContainer = document.getElementById("chat-container");
+    if (chatContainer) {
+      chatContainer.style.scrollBehavior = "smooth";
+      chatContainer.scrollTop = chatContainer.scrollHeight;
+    }
+  }, [messages]);
+
   return (
     <>
       <ChatHeader />
-      <div className="flex-1 px-6 overflow-y-auto py-8">
+      <div id="chat-container" className="flex-1 px-6 overflow-y-auto py-8">
         {messages.length > 0 && !isMessagesLoading ? (
           <div className="max-w-3xl mx-auto space-y-6">
             {messages.map((msg) => (
